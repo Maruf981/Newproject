f4c45883f431ad058796c61085a2fe8e1c9e92db
Maruf <marufabdulloev.888@mail.ru>
2024-11-20 17:26:07 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {screens: {
+      'sm': { 'max': '576px' },
+      // => @media (max-width: 576px) { ... }
+
+      'md': { 'max': '960px' },
+      // => @media (max-width: 960px) { ... }
+
+      'lg': { 'max': '1440px' },
+      // => @media (max-width: 1440px) { ... }
+    },
+  
+    backgroundImage: {
+      'hero-pattern': "url('/src/images/bg (2).png')",
+      'footer-texture': "url('/img/footer-texture.png')",},
+  },
+  plugins: [],
+}
+};
+
